627e467c85f440bf81bfe62f97e53912b559fab4
CSampebgo333 <clemsosampebgo7@gmail.com>
2024-06-27 05:23:27 +0000

    I include the code to further design the footer of the main page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-// Add any necessary JavaScript for dynamic behaviors here
-
-// Example: Button click interaction
 document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('mousedown', () => {
         button.style.backgroundColor = '#333';
         button.style.color = 'white';
+    });
+
+    button.addEventListener('mouseup', () => {
+        button.style.backgroundColor = '';
+        button.style.color = '';
+    });
+
+    button.addEventListener('mouseleave', () => {
+        button.style.backgroundColor = '';
+        button.style.color = '';
     });
 });
